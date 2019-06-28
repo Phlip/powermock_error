cb234723d829b41a646c2e1d22dcc07e806854a7
Phlip <phlip2005@gmail.com>
2019-06-28 12:00:03 -0700

    check in broken project to illustrate a bug

---
+++ MyApplication/app/src/androidTest/java/com/phlip/myapplication/ExampleInstrumentedTest.java
@@ -0,0 +1,34 @@
+package com.phlip.myapplication;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.easymock.PowerMock.createMock;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.phlip.myapplication", appContext.getPackageName());
+
+        // Now mock a BluetoothDevice:
+
+        BluetoothDevice mocked = createMock(BluetoothDevice.class);
+
+    }
+}
